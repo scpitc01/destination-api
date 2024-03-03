b8a10ce648847a1c392cbab8e12605a86e0ee256
Sean Pitchford <sean.c.pitchford@gmail.com>
2024-03-03 18:44:55 -0500

    Inserts destinations into mongo.

--- src/controllers/destination.ts
+++ src/controllers/destination.ts
@@ -2,6 +2,9 @@ import openCageService from '../services/opencage'
 import hereGeoCoding from '../services/hereGeocoding'
 import { AddDestinationObject } from '../types/objects/destination'
 import { OpenCageResults } from '../types/objects/opencage'
+import DestinationModel, { Destination } from '../models/destination';
+import { HereApiResults } from '../types/objects/here';
+
 
 
 class DestinationController {
@@ -12,31 +15,67 @@ class DestinationController {
     }
 
     public async insertLocation(destinationRequest: AddDestinationObject) {
-        const openCageResults: OpenCageResults = await openCageService.retrieveCoordinates(destinationRequest.city, destinationRequest.stateAbbreviation)
-        if (!openCageResults.results.length) {
-            throw new Error("No results found for the requested destination")
+        const destination = await DestinationModel.findOne({ city: destinationRequest.city, state: destinationRequest.stateAbbreviation })
+        if (destination) {
+            return destination
         }
-        const location = openCageResults.results[0]
-        const hereGeoCodingResturantResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'resturants')
-        const hereGeoCodingMuseumResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'museums')
-        const hereGeoCodingSkiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'ski')
-        const hereGeoCodingLeisureResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'leisure')
-        const hereGeoCodingNightLifeResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Nightlife-Entertainment')
-        const hereGeoCodingTheaterResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Theatre, Music and Culture')
-        const hereGeoCodingGamblingResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'casino')
-        const hereGeoCodingLandmarksResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Tourist Attraction')
-        const hereGeoCodingZooResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'zoo')
-        const hereGeoCodingAquariumResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'aquarium')
-        const hereGeoCodingStadiumResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'stadium')
-        const hereGeoCodingHotelResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Hotel or Motel')
-        const hereGeoCodingOutdoorResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Outdoor-Recreation')
-        const hereGeoCodingWaterResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Body of Water')
-        const hereGeoCodingHillsResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Mountain or Hill')
-        const hereGeoCodingForestResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Forest')
+        else {
+            const openCageResults: OpenCageResults = await openCageService.retrieveCoordinates(destinationRequest.city, destinationRequest.stateAbbreviation)
+            if (!openCageResults.results.length) {
+                throw new Error("No results found for the requested destination")
+            }
+            const location = openCageResults.results[0]
+            const hereGeoCodingResturantResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'resturants')
+            const hereGeoCodingMuseumResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'museums')
+            const hereGeoCodingSkiResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'ski')
+            const hereGeoCodingLeisureResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'leisure')
+            const hereGeoCodingNightLifeResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Nightlife-Entertainment')
+            const hereGeoCodingTheaterResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Theatre, Music and Culture')
+            const hereGeoCodingGamblingResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'casino')
+            const hereGeoCodingLandmarksResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Tourist Attraction')
+            const hereGeoCodingZooResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'zoo')
+            const hereGeoCodingAquariumResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'aquarium')
+            const hereGeoCodingStadiumResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'stadium')
+            const hereGeoCodingHotelResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Hotel or Motel')
+            const hereGeoCodingOutdoorResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Outdoor-Recreation')
+            const hereGeoCodingWaterResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Body of Water')
+            const hereGeoCodingHillsResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Mountain or Hill')
+            const hereGeoCodingForestResults: HereApiResults = await hereGeoCoding.retrievePointsOfInterest(location.geometry.lat, location.geometry.lng, 'Forest')
 
+            let combineOutdoorResults = hereGeoCodingOutdoorResults.items.concat(hereGeoCodingWaterResults.items)
+            combineOutdoorResults = combineOutdoorResults.concat(hereGeoCodingHillsResults.items)
+            combineOutdoorResults = combineOutdoorResults.concat(hereGeoCodingForestResults.items)
 
-        // const amadeusSafteyResults = await amadeusService.locationSafety(location.geometry.lat, location.geometry.lng)
-        // const amadeusActivityResults = await amadeusService.locationActivities(location.geometry.lat, location.geometry.lng)
+            const newDestination: Destination = {
+                city: destinationRequest.city,
+                state: destinationRequest.stateAbbreviation,
+                hasZoo: hereGeoCodingZooResults.items.length > 0,
+                hasSkiing: hereGeoCodingSkiResults.items.length > 0,
+                hasCasino: hereGeoCodingGamblingResults.items.length > 0,
+                hasSportStadium: hereGeoCodingStadiumResults.items.length > 0,
+                hasMuseum: hereGeoCodingMuseumResults.items.length > 0,
+                hasNightLife: hereGeoCodingNightLifeResults.items.length > 0,
+                hasAquarium: hereGeoCodingAquariumResults.items.length > 0,
+                hasBeach: hereGeoCodingLeisureResults.items.some(x => x.categories.some(x => x.name === 'Beach')),
+                hasMountains: hereGeoCodingHillsResults.items.length > 0,
+                hasOutdoorActivities: hereGeoCodingOutdoorResults.items.length > 0 || hereGeoCodingWaterResults.items.length > 0 || hereGeoCodingForestResults.items.length > 0,
+                hasArtisticsPlays: hereGeoCodingTheaterResults.items.length > 0,
+                hasAmusementPark: hereGeoCodingLandmarksResults.items.some(x => x.categories.some(x => x.name === 'Amusement Park')),
+                restaurantResults: hereGeoCodingResturantResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                museumResults: hereGeoCodingMuseumResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                skiingResults: hereGeoCodingSkiResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                nightLifeResults: hereGeoCodingNightLifeResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                theaterResults: hereGeoCodingTheaterResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                gamblingResults: hereGeoCodingGamblingResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                landMarkResults: hereGeoCodingLandmarksResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                zooResults: hereGeoCodingZooResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                aquariumResults: hereGeoCodingAquariumResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                stadiumResults: hereGeoCodingStadiumResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                hotelResults: hereGeoCodingHotelResults.items.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label })),
+                outDoorResults: combineOutdoorResults.map(x => ({ name: x.title, type: x.categories[0].name, address: x.address.label }))
+            }
+            await DestinationModel.create(newDestination)
+        }
     }
 }
 
--- src/models/destination.ts
+++ src/models/destination.ts
@@ -1,5 +1,10 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+export interface DestinationResult {
+    name: string,
+    type: string,
+    address: string
+}
 
 export interface Destination {
     city: string
@@ -7,12 +12,80 @@ export interface Destination {
     hasZoo: boolean,
     hasSkiing: boolean,
     hasCasino: boolean,
-    hasSportsStadium: boolean
+    hasSportStadium: boolean
+    hasMuseum: boolean,
+    hasNightLife: boolean,
+    hasAquarium: boolean,
+    hasBeach: boolean
+    hasMountains: boolean,
+    hasOutdoorActivities: boolean,
+    hasArtisticsPlays: boolean,
+    hasAmusementPark: boolean,
+    restaurantResults: DestinationResult[]
+    museumResults: DestinationResult[],
+    skiingResults: DestinationResult[],
+    nightLifeResults: DestinationResult[],
+    theaterResults: DestinationResult[],
+    gamblingResults: DestinationResult[],
+    landMarkResults: DestinationResult[],
+    zooResults: DestinationResult[],
+    aquariumResults: DestinationResult[],
+    stadiumResults: DestinationResult[],
+    hotelResults: DestinationResult[],
+    outDoorResults: DestinationResult[]
 }
 
 const destinationSchema = new Schema<Destination>({
     city: { type: String, required: true },
-    state: { type: String, required: true }
+    state: { type: String, required: true },
+    hasZoo: { type: Boolean, required: true, default: false },
+    hasSkiing: { type: Boolean, required: true, default: false },
+    hasCasino: { type: Boolean, required: true, default: false },
+    hasSportStadium: { type: Boolean, required: true, default: false },
+    hasMuseum: { type: Boolean, required: true, default: false },
+    hasNightLife: { type: Boolean, required: true, default: false },
+    hasAquarium: { type: Boolean, required: true, default: false },
+    hasBeach: { type: Boolean, required: true, default: false },
+    hasMountains: { type: Boolean, required: true, default: false },
+    hasOutdoorActivities: { type: Boolean, required: true, default: false },
+    hasArtisticsPlays: { type: Boolean, required: true, default: false },
+    hasAmusementPark: { type: Boolean, required: true, default: false },
+    restaurantResults: {
+        type: [], required: false
+    },
+    museumResults: {
+        type: [], required: false
+    },
+    skiingResults: {
+        type: [], required: false
+    },
+    nightLifeResults: {
+        type: [], required: false
+    },
+    theaterResults: {
+        type: [], required: false
+    },
+    gamblingResults: {
+        type: [], required: false
+    },
+    landMarkResults: {
+        type: [], required: false
+    },
+    zooResults: {
+        type: [], required: false
+    },
+    aquariumResults: {
+        type: [], required: false
+    },
+    stadiumResults: {
+        type: [], required: false
+    },
+    hotelResults: {
+        type: [], required: false
+    },
+    outDoorResults: {
+        type: [], required: false
+    }
 
 });
 
