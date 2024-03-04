fa3041cfe0745f0638180e9a0a2bc9da57083ece
Sean Pitchford <sean.c.pitchford@gmail.com>
2024-03-04 18:43:34 -0500

    Fixed openapi

--- src/swaggerSchemas/destination.ts
+++ src/swaggerSchemas/destination.ts
@@ -21,7 +21,35 @@ export const addDestinationObjectRequest = {
     response: {
         200: {
             description: 'Successful response',
-            type: 'object'
+            type: 'object',
+            properties: {
+                _id: { type: 'string' },
+                city: { type: 'string' },
+                hasZoo: { type: 'boolean' },
+                hasSkiing: { type: 'boolean' },
+                hasCasino: { type: 'boolean' },
+                hasSportStadium: { type: 'boolean' },
+                hasMuseum: { type: 'boolean' },
+                hasNightLife: { type: 'boolean' },
+                hasAquarium: { type: 'boolean' },
+                hasBeach: { type: 'boolean' },
+                hasMountains: { type: 'boolean' },
+                hasOutdoorActivities: { type: 'boolean' },
+                hasArtisticsPlays: { type: 'boolean' },
+                hasAmusementPark: { type: 'boolean' },
+                restaurantResults: { type: 'array' },
+                museumResults: { type: 'array' },
+                skiingResults: { type: 'array' },
+                nightLifeResults: { type: 'array' },
+                theaterResults: { type: 'array' },
+                gamblingResults: { type: 'array' },
+                landMarkResults: { type: 'array' },
+                zooResults: { type: 'array' },
+                aquariumResults: { type: 'array' },
+                stadiumResults: { type: 'array' },
+                hotelResults: { type: 'array' },
+                outDoorResults: { type: 'array' }
+            }
         },
     },
 };
