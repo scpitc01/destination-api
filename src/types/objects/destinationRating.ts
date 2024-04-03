f9fe5c1c36b9c732c557d674bc603503a61e8e12
Sean Pitchford <sean.c.pitchford@gmail.com>
2024-04-02 22:23:25 -0400

    New type for array.

--- src/types/objects/destinationRating.ts
+++ src/types/objects/destinationRating.ts
@@ -1,4 +1,10 @@
+import { Destination } from "../../models/destination";
+
 export interface FindDestinationRating {
     userId: string,
     destinationId: string
+}
+
+export interface DestinationRatingWithDestination extends Destination {
+    rating: number
 }
