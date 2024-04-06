bf9f6553480f4b98738c0b8202610c7d6606055b
Sean Pitchford <sean.c.pitchford@gmail.com>
2024-04-06 19:43:15 -0400

    Finished destination controller tests.

--- __tests__/controllers/destination.test.ts
+++ __tests__/controllers/destination.test.ts
@@ -1,6 +1,8 @@
 import { Schema } from 'mongoose';
 import destinationController from '../../src/controllers/destination'
 import destinationModel from '../../src/models/destination';
+import opencageService from '../../src/services/opencage';
+import hereGeocodingService from '../../src/services/hereGeocoding';
 
 
 describe('Destination Controller', () => {
@@ -24,5 +26,49 @@ describe('Destination Controller', () => {
             expect(err).toBeDefined()
             expect(serviceFindDestination).toHaveBeenCalled()
         }
+    })
+
+    it('insertLocation should respond with a destination in mongo', async () => {
+        const findDestination = jest.fn().mockResolvedValue({ _id: 'Test' });
+        const serviceFindDestination = jest.spyOn(destinationModel, 'findOne').mockImplementation(findDestination);
+        const result = await DestinationController.insertLocation({ city: 'Louisville', stateAbbreviation: 'KY' })
+
+        expect(serviceFindDestination).toHaveBeenCalled()
+        expect(result._id).toBe('Test')
+    })
+
+    it('insertLocation should fail with no useable reponse from opencage', async () => {
+        const findDestination = jest.fn().mockResolvedValue(null);
+        const serviceFindDestination = jest.spyOn(destinationModel, 'findOne').mockImplementation(findDestination);
+        const retrieveCoordinates = jest.fn().mockResolvedValue({ results: null });
+        const serviceRetrieveCoordinates = jest.spyOn(opencageService, 'retrieveCoordinates').mockImplementation(retrieveCoordinates);
+
+        try {
+            const result = await DestinationController.insertLocation({ city: 'Louisville', stateAbbreviation: 'KY' })
+        }
+        catch (err) {
+            expect(err).toBeDefined()
+        }
+
+        expect(serviceFindDestination).toHaveBeenCalled()
+        expect(serviceRetrieveCoordinates).toHaveBeenCalled()
+    })
+
+    it('insertLocation should succeed', async () => {
+        const findDestination = jest.fn().mockResolvedValue(null);
+        const serviceFindDestination = jest.spyOn(destinationModel, 'findOne').mockImplementation(findDestination);
+        const retrieveCoordinates = jest.fn().mockResolvedValue({ results: [{ geometry: { lat: 30, ln: 40 } }] });
+        const serviceRetrieveCoordinates = jest.spyOn(opencageService, 'retrieveCoordinates').mockImplementation(retrieveCoordinates);
+        const retrievePointsOfInterest = jest.fn().mockResolvedValue({ items: [{ title: 'Test1', distance: 50, categories: [{ name: 'Test', primary: true }], address: { label: 'Test Address' } }] });
+        const serviceRetrievePointsOfInterest = jest.spyOn(hereGeocodingService, 'retrievePointsOfInterest').mockImplementation(retrievePointsOfInterest);
+        const createDestination = jest.fn().mockResolvedValue(null);
+        const serviceCreateDestination = jest.spyOn(destinationModel, 'create').mockImplementation(createDestination);
+
+        const result = await DestinationController.insertLocation({ city: 'Louisville', stateAbbreviation: 'KY' })
+
+        expect(serviceFindDestination).toHaveBeenCalled()
+        expect(serviceRetrieveCoordinates).toHaveBeenCalled()
+        expect(serviceRetrievePointsOfInterest).toHaveBeenCalled()
+        expect(serviceCreateDestination).toHaveBeenCalled()
     })
 })
