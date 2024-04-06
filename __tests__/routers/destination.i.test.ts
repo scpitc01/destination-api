2e6fee0bf771e226068458527cfd26bfcf95bdf4
Sean Pitchford <sean.c.pitchford@gmail.com>
2024-04-06 19:59:54 -0400

    Destination intergration test done.

--- __tests__/routers/destination.i.test.ts
+++ __tests__/routers/destination.i.test.ts
@@ -1,7 +1,9 @@
 import supertest from 'supertest'
 import app from '../../src/app' // Import your Fastify application
 import { App } from 'supertest/types'
-import DestinationModel from '../../src/models/destination'
+import destinationModel from '../../src/models/destination';
+import opencageService from '../../src/services/opencage';
+import hereGeocodingService from '../../src/services/hereGeocoding';
 import authenticationService from '../../src/services/authentication'
 
 describe('Destination Rating Router', () => {
@@ -16,7 +18,7 @@ describe('Destination Rating Router', () => {
 
     it('GET /destination/:id/ responds with a 200 and returns object.', async () => {
         const findDestination = jest.fn().mockResolvedValue({ city: 'Louisville', state: 'KY', _id: 'TestID' })
-        const serviceFindDestination = jest.spyOn(DestinationModel, 'findById').mockImplementation(findDestination)
+        const serviceFindDestination = jest.spyOn(destinationModel, 'findById').mockImplementation(findDestination)
         // Create a Supertest request instance
         const request = supertest(server)
 
@@ -26,6 +28,33 @@ describe('Destination Rating Router', () => {
         // Assert that the response status code is 200
         expect(response.status).toBe(200)
         expect(serviceFindDestination).toHaveBeenCalled()
+    })
+
+    it('POST /destination responds with a 200 and returns object.', async () => {
+        const findDestination = jest.fn().mockResolvedValue(null);
+        const serviceFindDestination = jest.spyOn(destinationModel, 'findOne').mockImplementation(findDestination);
+        const retrieveCoordinates = jest.fn().mockResolvedValue({ results: [{ geometry: { lat: 30, ln: 40 } }] });
+        const serviceRetrieveCoordinates = jest.spyOn(opencageService, 'retrieveCoordinates').mockImplementation(retrieveCoordinates);
+        const retrievePointsOfInterest = jest.fn().mockResolvedValue({ items: [{ title: 'Test1', distance: 50, categories: [{ name: 'Test', primary: true }], address: { label: 'Test Address' } }] });
+        const serviceRetrievePointsOfInterest = jest.spyOn(hereGeocodingService, 'retrievePointsOfInterest').mockImplementation(retrievePointsOfInterest);
+        const createDestination = jest.fn().mockResolvedValue(null);
+        const serviceCreateDestination = jest.spyOn(destinationModel, 'create').mockImplementation(createDestination);
+        // Create a Supertest request instance
+        const request = supertest(server)
+
+        // Send a GET request to the specified route
+        const response = await request.post('/destination').send({
+            "city": 'Louisville',
+            "stateAbbreviation": 'KY'
+        })
+
+        // Assert that the response status code is 200
+        expect(response.status).toBe(200)
+        expect(serviceFindDestination).toHaveBeenCalled()
+        expect(serviceFindDestination).toHaveBeenCalled()
+        expect(serviceRetrieveCoordinates).toHaveBeenCalled()
+        expect(serviceRetrievePointsOfInterest).toHaveBeenCalled()
+        expect(serviceCreateDestination).toHaveBeenCalled()
     })
 
     afterAll(async () => {
